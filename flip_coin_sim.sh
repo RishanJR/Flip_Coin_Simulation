a8a1f19028898625a88dc3bc6a890d090ac2a4ff
RishanJR <rishanrajkumar@yahoo.com>
2020-09-22 15:22:21 +0400

    Checking n number of cases

--- flip_coin_sim.sh
+++ flip_coin_sim.sh
@@ -3,14 +3,28 @@
 echo -e "\nWelcome to Flip Coin Simulation"
 echo -e "This problem displays the winner, Heads or Tails\n"
 
-#Simulating heads or tails using random function
-res=$(( RANDOM%2 ))
+#Variable
+count=0
+hcount=0
+tcount=0
 
-#0 for heads and 1 for tails
-if [[ res -eq 0 ]]
-then
-	echo Heads won
-else
-	echo Tails won
-fi
+echo Enter the number of times you want to check
+read n
 
+while [[ $count -lt n ]]
+do
+	#Simulating heads or tails using random function
+	res=$(( RANDOM%2 ))
+
+	#0 for heads and 1 for tails
+	if [[ res -eq 0 ]]
+	then
+		((hcount++))
+	else
+		((tcount++))
+	fi
+	((count++))
+done
+
+echo -e "\nNumber of times Heads won is $hcount"
+echo Number of times Tails won is $tcount
