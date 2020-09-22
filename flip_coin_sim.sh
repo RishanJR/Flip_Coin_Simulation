8813f830a4f8e45e4263b8a45b4f478ffd631640
RishanJR <rishanrajkumar@yahoo.com>
2020-09-22 15:14:38 +0400

    Using RANDOM to check if it is heads or tails

--- flip_coin_sim.sh
+++ flip_coin_sim.sh
@@ -3,3 +3,14 @@
 echo -e "\nWelcome to Flip Coin Simulation"
 echo -e "This problem displays the winner, Heads or Tails\n"
 
+#Simulating heads or tails using random function
+res=$(( RANDOM%2 ))
+
+#0 for heads and 1 for tails
+if [[ res -eq 0 ]]
+then
+	echo Heads won
+else
+	echo Tails won
+fi
+
